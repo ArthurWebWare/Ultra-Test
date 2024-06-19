c1559456e8a73b4a767ced0657eeb4831866b996
Arthur Lipciu <arthur.webware@gmail.com>
2024-06-20 00:19:51 +0300

    update routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\PostController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
 Route::middleware('auth:api')->group(function () {
     Route::get('posts', [PostController::class, 'index']);
+    Route::get('post/{id}', [PostController::class, 'show']);
+    Route::post('post', [PostController::class, 'store']);
+    Route::put('post/{id}', [PostController::class, 'update']);
+    Route::delete('post/{id}', [PostController::class, 'destroy']);
+
     Route::get('users/{user}/posts', [PostController::class, 'userPosts']);
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
